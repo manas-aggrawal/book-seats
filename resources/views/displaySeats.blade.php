8ba21711fc29eb34a859a50bcc012ff7fb93f046
manas-aggrawal <manasagg7199@gmail.com>
2022-10-17 17:34:23 +0530

    error message on booking more than 7 seats in one go

--- resources/views/displaySeats.blade.php
+++ resources/views/displaySeats.blade.php
@@ -9,6 +9,11 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
+                    @if (session()->has('message'))
+                        <div class="alert alert-success" style="color: red;">
+                            {{ session()->get('message') }}
+                        </div>
+                    @endif
 
                     <div>
                         <form action="{{ route('select.seats') }}" enctype="multipart/form-data" method="post">
